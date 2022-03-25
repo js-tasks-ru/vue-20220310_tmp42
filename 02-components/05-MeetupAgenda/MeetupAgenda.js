9b156e4240e5861096d40af3f05ee82df7416d57
tmp42 <tmp42@mail.ru>
2022-03-25 22:35:33 +0300

    Created module 2

--- 02-components/05-MeetupAgenda/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/MeetupAgenda.js
@@ -1,13 +1,20 @@
-import { defineComponent } from './vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import {defineComponent} from './vendor/vue.esm-browser.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
+
+  components: {
+    MeetupAgendaItem,
+  },
 
   template: `
     <ul class="agenda">
       <li class="agenda__item">
         <!-- meetup agenda item -->
+        <div v-for="item in agenda">
+          <meetup-agenda-item :agenda-item="item"></meetup-agenda-item>
+        </div>
       </li>
     </ul>`,
 });
