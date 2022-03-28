a812ad71fd15a3af46aeac525a18cc4968eb097d
tmp42 <tmp42@mail.ru>
2022-03-28 19:42:30 +0300

    create module 3

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+    <h1 class="meetup-cover__title">{{title}}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MeetupCover',
+
+  props: {
+    title: String,
+    image: String,
+  },
+  computed: {
+    getBackground() {
+      let imgSrc='var(--default-cover)';
+      if(this.image){
+        imgSrc='url('+this.image+')';
+      }
+      return imgSrc;
+    },
+  },
 };
 </script>
 
@@ -14,9 +28,7 @@ export default {
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
-  /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
+  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), v-bind(getBackground);
   display: flex;
   flex-direction: column;
   align-items: center;
